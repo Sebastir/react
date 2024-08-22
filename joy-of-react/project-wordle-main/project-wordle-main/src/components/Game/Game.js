371150ddb185923ff72a86fc1b1392dec8771f8b
Seb Rojas <src.py@icloud.com>
2024-08-22 16:37:05 +0200

    new materials

---
+++ joy-of-react/project-wordle-main/project-wordle-main/src/components/Game/Game.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import { sample } from '../../utils';
+import { WORDS } from '../../data';
+import GuessInput from '../GuessInput'
+import GuessResults from '../GuessResults'
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import LostBanner from '../LostBanner/';
+import WonBanner from '../WonBanner';
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
+
+function Game() {
+const [gameStatus, setGameStatus] = React.useState('running')
+  const [guesses, setGuesses] = React.useState([]);
+
+
+function handleSubmitGuess(tentativeGuess) {
+  const nextGuesses = [...guesses, tentativeGuess]
+  setGuesses(nextGuesses);
+
+  if (tentativeGuess.toUpperCase() === answer) {
+    setGameStatus('won')
+  } else if(nextGuesses.length >= NUM_OF_GUESSES_ALLOWED ) {
+    setGameStatus('lost')
+  }
+}
+  return (
+  <>
+    <GuessResults
+      guesses={guesses}
+      answer={answer}
+    />
+    <GuessInput
+      handleSubmitGuess={handleSubmitGuess}
+      gameStatus={gameStatus}
+    />
+  {gameStatus === 'won' && (
+    <WonBanner
+    numOfGuesses={guesses.length}
+    />
+  )}
+    {gameStatus === 'lost' && (
+    <LostBanner
+    answer={answer}
+    />
+  )}
+</>
+);
+}
+
+export default Game;
